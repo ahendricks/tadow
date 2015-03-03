52d8e8518202b27a600954c21945a9a6b3479449
Alex Hendricks <alex@hendricksmail.com>
2015-03-02 19:52:38 -0800

    User can add task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,23 @@
 class TasksController < ApplicationController
   def index
   	@tasks = Task.all
+  	@new_task = Task.new
+  end
+
+  def create
+  	@task = Task.new(task_params)
+  	if @task.save
+  		flash[:success] = "Task successfully saved."
+  		redirect_to root_path
+  	else
+  		flash[:warning] = "Task not saved."
+  		redirect_to root_path
+  	end
   end
 end
+
+private
+
+	def task_params
+		params.require(:task).permit(:name)
+	end
